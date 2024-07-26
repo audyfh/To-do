fa53b23d031cd4fc60b8fc467395e3d042d6e047
audyfh <155510215+audyfh@users.noreply.github.com>
2024-07-26 15:11:25 +0700

    add splash screen

--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import com.example.mytodo.ui.theme.MyTodoTheme
 
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MyTodoTheme {
                 // A surface container using the 'background' color from the theme
